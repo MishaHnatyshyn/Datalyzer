ad6ea834a7040b946cae6d19da19659e04613a4c
Misha Afanasiuk <mike.afanasiuk.21@gmail.com>
2019-11-01 13:30:37 +0200

    Fixed pr issue Moved Popup examples to another dir

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Login from '../pages/Login';
-import PopupExamples from './PopupExamples';
 
 const App = () => (
   <div>
-    <PopupExamples />
     <Switch>
       <Route exact path="/" component={Login} />
       <Route path="/login" component={Login} />
