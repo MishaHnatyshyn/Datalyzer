8779a1df3e41f98ae20202b8d9ab15140e41519e
Alexandra Delieva <31667373+delieva@users.noreply.github.com>
2019-11-25 21:52:12 +0200

    Update selectors.js

--- src/store/user/selectors.js
+++ src/store/user/selectors.js
@@ -1,7 +1,7 @@
 import { compose, prop } from 'lodash/fp';
 
 const root = (state) => state.user;
-compose(prop('username'), root);
+export const getUsername = compose(prop('username'), root);
 export const getUserType = compose(prop('userType'), root);
 export const getUserId = compose(prop('userId'), root);
 export const getUser = root;
