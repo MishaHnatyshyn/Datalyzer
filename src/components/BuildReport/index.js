6019459f0b242335ee4327a1d32bbd245645c8f0
VladaZhmurkevych <vladagmyrka1@gmail.com>
2019-12-15 14:46:58 +0200

    WIP

--- src/components/BuildReport/index.js
+++ src/components/BuildReport/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import styles from './buildReport.module.scss';
 import ReportDataSection from '../ReportDataSection';
+import NewReportButtonsContainer from '../NewReportButtonsContainer';
 
 const BuildReport = () => (
   <div className={styles.container}>
     <ReportDataSection />
+    <NewReportButtonsContainer classes={styles.stepsButton} />
   </div>
 );
 
